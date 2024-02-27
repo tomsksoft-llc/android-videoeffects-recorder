802352dda3d37f63d454c50a6fa4bf1a5d6ea59a
Exynos <sinerechka0000@gmail.com>
2024-02-27 20:18:34 +0700

    don't save empty video record broken files

--- data/src/main/java/com/tomsksoft/videoeffectsrecorder/data/VideoRecorderImpl.kt
+++ data/src/main/java/com/tomsksoft/videoeffectsrecorder/data/VideoRecorderImpl.kt
@@ -1,5 +1,6 @@
 package com.tomsksoft.videoeffectsrecorder.data
 
+import android.annotation.SuppressLint
 import android.content.ContentResolver
 import android.content.ContentValues
 import android.content.Context
@@ -7,10 +8,12 @@ import android.graphics.Color
 import android.graphics.PorterDuff
 import android.graphics.Rect
 import android.media.MediaRecorder
+import android.net.Uri
 import android.os.Build
 import android.os.Environment
 import android.os.ParcelFileDescriptor
 import android.provider.MediaStore
+import android.util.Log
 import android.view.Surface
 import com.tomsksoft.videoeffectsrecorder.domain.VideoRecorder
 import io.reactivex.rxjava3.disposables.Disposable
@@ -24,6 +27,9 @@ class VideoRecorderImpl(
     private val context: Context,
     private val directoryName: String
 ): VideoRecorder {
+    companion object {
+        private const val TAG = "VideoRecorder"
+    }
 
     override val frame: BehaviorSubject<Any> = BehaviorSubject.create()
     override val degree: BehaviorSubject<Int> = BehaviorSubject.create()
@@ -39,29 +45,48 @@ class VideoRecorderImpl(
         get() = context.contentResolver
 
     override fun startRecord(filename: String, extension: String, mimeType: String): AutoCloseable =
-        Record(createOutput(filename, extension, mimeType))
+        open(filename, extension, mimeType).let { descToFile: Pair<ParcelFileDescriptor, File> ->
+            Record(descToFile.first, descToFile.second)
+        }
 
-    private fun createOutput(
+    private fun open(
         filename: String,
         extension: String,
         mimeType: String
-    ): ParcelFileDescriptor =
+    ): Pair<ParcelFileDescriptor, File> =
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q)
-            ParcelFileDescriptor.open(
-                createFile(filename, extension),
-                ParcelFileDescriptor.MODE_WRITE_ONLY
-            )
-        else
-            createFileDescriptor(filename, mimeType)
+            createFile(filename, extension).let { file ->
+                ParcelFileDescriptor.open(
+                    file,
+                    ParcelFileDescriptor.MODE_WRITE_ONLY
+                ) to file
+            } else createFileDescriptor(filename, mimeType)
 
-    private fun createFileDescriptor(filename: String, mimeType: String): ParcelFileDescriptor {
+    @SuppressLint("Recycle") // ParcelFileDescriptor will be freed by Record
+    private fun createFileDescriptor(filename: String, mimeType: String): Pair<ParcelFileDescriptor, File> {
         val uri = contentResolver.insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, ContentValues().apply {
             put(MediaStore.Video.Media.DATE_ADDED, System.currentTimeMillis())
             put(MediaStore.Video.Media.DISPLAY_NAME, filename)
             put(MediaStore.Video.Media.MIME_TYPE, mimeType)
             put(MediaStore.Video.Media.RELATIVE_PATH, "${Environment.DIRECTORY_DCIM}/$directoryName")
         })!! // MediaStore concerns about making dirs and providing unique filenames on its own
-        return contentResolver.openFileDescriptor(uri, "w")!!
+        return contentResolver.openFileDescriptor(uri, "w")!! to
+                File(File(
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM),
+                    directoryName
+                ), getFilename(uri))
+    }
+
+    private fun getFilename(uri: Uri): String = contentResolver.query(
+        uri,
+        arrayOf(MediaStore.Video.Media.DISPLAY_NAME),
+        null, null
+    )!!.use { cursor ->
+        cursor.moveToFirst()
+        cursor.getString(
+            cursor.getColumnIndex(MediaStore.Video.Media.DISPLAY_NAME)
+                .takeUnless { it == -1 }!!
+        )
     }
 
     private fun createFile(filename: String, extension: String): File =
@@ -92,10 +117,11 @@ class VideoRecorderImpl(
     }
 
     private inner class Record(
-        private val outputFile: ParcelFileDescriptor
+        private val parcelDescriptor: ParcelFileDescriptor,
+        private val file: File // TODO [tva] if there's a way to delete file via ParcelFileDescriptor, do so
     ): AutoCloseable {
         @Volatile
-        private var recordState: RecordState = RecordState.CREATED
+        private var recordState: RecordState = RecordState.IDLE
         private val mediaRecorder = (
                 if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S)
                     MediaRecorder()
@@ -106,6 +132,7 @@ class VideoRecorderImpl(
         private var disposableFrameSubscription: Disposable? = null
 
         init {
+            Log.d(TAG, "Record output: ${file.absolutePath}")
             disposableFrameSubscription = frame.observeOn(Schedulers.io()).subscribe { frame ->
                 val bitmap = FrameMapper.fromAny(frame)
                 val (width, height) = bitmap.width to bitmap.height
@@ -124,12 +151,13 @@ class VideoRecorderImpl(
         }
 
         override fun close() {
+            Log.d(TAG, "Interrupt video record in state: $recordState")
             when (recordState) {
-                RecordState.CREATED, RecordState.STOPPING -> {
+                RecordState.IDLE -> {
                     recordState = RecordState.STOPPED
                     disposableFrameSubscription?.dispose()
                     mediaRecorder.release()
-                    outputFile.close()
+                    parcelDescriptor.close()
                 }
 
                 RecordState.STARTING -> // start() will check if STOPPING was requested
@@ -140,7 +168,15 @@ class VideoRecorderImpl(
                     disposableFrameSubscription?.dispose()
                     mediaRecorder.stop()
                     mediaRecorder.release()
-                    outputFile.close()
+                    parcelDescriptor.close()
+                }
+
+                RecordState.STOPPING -> { // MediaRecorder wasn't started, so there's no frames recorded
+                    recordState = RecordState.STOPPED
+                    disposableFrameSubscription?.dispose()
+                    mediaRecorder.release()
+                    parcelDescriptor.close()
+                    file.delete() // video without frames would represent broken file
                 }
 
                 RecordState.STOPPED ->
@@ -167,7 +203,7 @@ class VideoRecorderImpl(
                 setVideoSize(width, height)
 
                 setOrientationHint((360 - (degree.value ?: 0)) % 360)
-                setOutputFile(outputFile.fileDescriptor)
+                setOutputFile(parcelDescriptor.fileDescriptor)
 
                 prepare()
             }
@@ -183,10 +219,10 @@ class VideoRecorderImpl(
     }
 
     private enum class RecordState {
-        CREATED,
-        STARTING,
-        RECORDING,
-        STOPPING,
+        IDLE, // waiting for first frame to setup MediaRecorder
+        STARTING, // setting up MediaRecorder
+        RECORDING, // MediaRecorder catches frames
+        STOPPING, // waiting for finish setting up MediaRecorder to release after
         STOPPED
     }
 }
